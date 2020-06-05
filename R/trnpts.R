e998afc926164444a953dafb978743ea44f23634
fawda123 <mbafs2012@gmail.com>
2020-06-05 16:42:43 -0400

    filter on character

--- R/trnpts.R
+++ R/trnpts.R
@@ -10,7 +10,7 @@
 #' 
 #' trnpts <- st_read('T:/05_GIS/SEAGRASS_TRANSECTS/TransectBasics2019.shp') %>% 
 #'    st_transform(crs = 4326) %>% 
-#'    dplyr::filter(!TRAN_ID %in% c('S8T1', 'S8T2', 'S8T3'))
+#'    dplyr::filter(!as.character(TRAN_ID) %in% c('S8T1', 'S8T2', 'S8T3'))
 #' 
 #' save(trnpts, file = 'data/trnpts.RData', compress = 'xz')
 #' }
