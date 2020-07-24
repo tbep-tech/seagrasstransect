c4249e3cfcf10c4e52b6fafb929e8cff2ab76d0c
fawda123 <mbafs2012@gmail.com>
2020-07-24 17:30:55 -0400

    adding stats but dont use lm

--- R/funcs.R
+++ R/funcs.R
@@ -5,6 +5,7 @@ pal_alg <- colorFactor(
   levels = c('Bacillariophyta', 'Cyanobacteria', 'Karenia brevis', 'Pseudo-nitzschia pungens', 'Pseudo-nitzschia sp.', 'Pyrodinium bahamense', 'Tripos hircus', 'other')
 )
 
+# p value text formatting
 p_ast <- function(x){
   
   sig_cats <- c('**', '*', '')
@@ -17,3 +18,37 @@ p_ast <- function(x){
   return(out)
   
 }
+
+# vertical lines in plots
+vrtline_fun <- function(datin, sgty){
+  
+  dts <- datin %>% 
+    group_by(Date) %>% 
+    summarise(val = sum(val)) %>% 
+    ungroup()
+  
+  if(sgty == 'Freq. Occ.')
+    dts$val <- 100
+  
+  out <- list()
+
+  for(i in 1:nrow(dts)){
+    
+    outi <- list(list(
+      type = "line",
+      y0 = 0,
+      y1 = dts[[i, 'val']],
+      xref = "x",
+      x0 = dts[[i, 'Date']],
+      x1 = dts[[i, 'Date']],
+      line = list(color = 'grey', width = 0.5, opacity = 0.5)
+    ))
+    
+    out <- c(out, outi)
+    
+  }
+
+  return(out)
+    
+}
+  
